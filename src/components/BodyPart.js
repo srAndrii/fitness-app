017b62a15fa4995ce8f2383e946678da10a4d57c
srAndrii <55akif5548@gmail.com>
2022-06-23 13:41:28 +0300

    Added Body Part component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
-const BodyPart = () => {
+
+import Icon from '../assets/icons/gym.png'
+
+
+const BodyPart = ({item, setBodyPart, bodyPart}) => {
     return (
-        <div>BodyParts</div>
+        <Stack
+            type='button'
+            alignItems='center'
+            justifyContent='center'
+            className='bodyPart-card'
+            sx={{
+                borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+                backgroundColor: '#fff',
+                borderBottomLeftRadius: '20px',
+                width:'270px',
+                height: '280px',
+                cursor: 'pointer',
+                gap:'47px'   
+            }}
+            onClick={() => {
+                setBodyPart(item)
+                window.scrollTo({top:1800, left:100, behavior:'smooth'})
+            }}
+        >
+            <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+            <Typography
+                fontSize='24px'
+                fontWeight='bold'
+                color='#3a1212'
+                textTransform='capitalize'
+            >
+                {item}
+            </Typography>
+        </Stack>
     )
 }
 
