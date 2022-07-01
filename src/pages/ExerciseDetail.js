7f2897d7d81fccd260272b153332c4aa8d5ccaed
srAndrii <55akif5548@gmail.com>
2022-07-01 19:52:38 +0300

    Added Detail Component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -18,7 +18,7 @@ const ExerciseDetail = () => {
             const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
             const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
 
-            const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exrecisesOptions)
+            const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exrecisesOptions)
             setExerciseDetail(exerciseDetailData)
         }
         fetchExercisesData()
