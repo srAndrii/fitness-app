f3c3799a9c748e2bfc3bab6770b20f0f5b87e45c
srAndrii <55akif5548@gmail.com>
2022-07-04 19:13:12 +0300

    Added Similar Exercises Componnet and Loader

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,32 +1,37 @@
-import { Box, Typography } from '@mui/material'
 import React, { useContext } from 'react'
+
+import { Box, Typography } from '@mui/material'
+
 import BodyPart from './BodyPart'
+
+import ExerciseCard from './ExerciseCard'
+
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+	const { scrollPrev } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
+	return (
+		<Typography onClick={() => scrollPrev()} className="right-arrow">
+			<img src={LeftArrowIcon} alt="right-arrow" />
+		</Typography>
+	);
 };
 
 const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
+	const { scrollNext } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
+	return (
+		<Typography onClick={() => scrollNext()} className="left-arrow">
+			<img src={RightArrowIcon} alt="right-arrow" />
+		</Typography>
+	);
 };
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
     
 
     return (
@@ -38,7 +43,8 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
                     title={item.id || item}
                     m='0 40px'
                 >
-                    <BodyPart item={item } bodyPart={bodyPart} setBodyPart={setBodyPart}  />
+					{isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+						: <ExerciseCard exercise={item} />}
                 </Box>))}
         </ScrollMenu>
     )
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -12,6 +12,8 @@ import SimilarExercises from "../components/SimilarExercises"
 const ExerciseDetail = () => {
     const [exerciseDetail, setExerciseDetail] = useState({})
     const [exerciseVideos, setExerciseVideos] = useState([])
+    const [targetMuscleExercises, setTargetMuscleExercises] = useState([])
+    const [equipmentExercises, setEquipmentExercises] = useState([])
     const { id } = useParams()
     
     useEffect(() => {
@@ -24,6 +26,12 @@ const ExerciseDetail = () => {
 
             const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
             setExerciseVideos(exerciseVideosData.contents)
+
+            const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exrecisesOptions)
+            setTargetMuscleExercises(targetMuscleExercisesData)
+
+            const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exrecisesOptions)
+            setEquipmentExercises(equipmentExercisesData)
         }
         fetchExercisesData()
     }, [id])
@@ -32,7 +40,7 @@ const ExerciseDetail = () => {
         <Box>
             <Detail exerciseDetail={exerciseDetail} />
             <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-            <SimilarExercises />
+            <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
         </Box>
     )
 }
